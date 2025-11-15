10d9d5d0b11d3e93573f954b29c24059e10e681f
Priyanshu312003 <priyanshumaurya.2003@gmail.com>
2025-11-15 09:09:22 +0530

    fix tailwind installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 function App() {
   
   return (
-    <>
-      
-    </>
+    <div className="bg-black min-h-screen">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem dolores, ipsum, beatae sequi ipsa recusandae debitis laborum temporibus commodi quasi molestiae, doloremque similique harum dignissimos distinctio veritatis!
+      {/* <div className="relative pt-20">
+       hii
+      </div> */}
+    </div>
   )
 }
 
