182e97aea6eca2af0002010f08dd132c422868bf
Priyanshu312003 <priyanshumaurya.2003@gmail.com>
2025-12-04 01:08:56 +0530

    import logo in constants.js file

---
+++ src/constants.js
@@ -0,0 +1,65 @@
+// Skill section logo
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import jsLogo from './assets/tech_logo/javascript.png';
+import reactLogo from './assets/tech_logo/reactjs.png';
+import tailwindLogo from './assets/tech_logo/tailwindcss.png';
+
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+
+export const SkillsInfo = [
+    {
+        title: 'Frontend',
+        skills: [
+            { name: 'HTML', logo: htmlLogo },
+            { name: 'CSS', logo: cssLogo },
+            { name: 'JAVASCRIPT', logo: jsLogo },
+            { name: 'REACTJS', logo: reactLogo },
+            { name: 'TAILWIND CSS', logo: tailwindLogo },
+        ],
+    },
+    {
+        title: 'Backend',
+        skills: [
+            { name: "NODEJS", logo: nodejsLogo },
+            { name: "EXPRESSJS", logo: expressjsLogo },
+            { name: "MYSQL", logo: mysqlLogo },
+            { name: "MONGODB", logo: mongodbLogo },
+        ],
+    },
+    {
+        title: 'Languages',
+        skills: [
+            { name: 'C', logo: cLogo },
+            { name: 'C++', logo: cppLogo },
+            { name: 'JAVASCRIPT', logo: javascriptLogo },
+            { name: 'PYTHON', logo: pythonLogo },
+            { name: 'TYPESCRIPT', logo: typescriptLogo },
+        ],
+    },
+    {
+        title: 'Tools',
+        skills: [
+            { name: 'GIT', logo: gitLogo },
+            { name: 'GITHUB', logo: githubLogo },
+            { name: 'VS CODE', logo: vscodeLogo },
+            { name: 'POSTMAN', logo: postmanLogo },
+            { name: 'COMPASS', logo: mcLogo },
+        ],
+    }
+]
